83054c94106d7993720ef2e9e3f3ad089fb98772
Maurice Svay <maurice@svay.com>
2023-04-23 23:46:28 +0200

    fix ref

--- src/editor/CenterButton.tsx
+++ src/editor/CenterButton.tsx
@@ -14,7 +14,7 @@ const POSITION_CLASSES = {
 
 export function CenterButton({ waypoints }: { waypoints: Waypoint[] }) {
   const parentMap = useMap();
-  const buttonRef = useRef();
+  const buttonRef = useRef<HTMLButtonElement>();
 
   const handleCenterMap: MouseEventHandler<HTMLButtonElement> = () => {
     // Compute bounds
