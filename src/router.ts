963c89301e498bbaca7677813b9a2e8a54f93c09
Maurice Svay <maurice@svay.com>
2023-04-23 21:50:25 +0200

    Update router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,14 @@
 import { createRouter } from "@swan-io/chicane/dist/index.js";
 
-export const Router = createRouter({
-  Home: "/",
-  Editor: "/editor",
-  About: "/about",
-  UserList: "/users",
-  UserDetail: "/users/:userId",
-});
+export const Router = createRouter(
+  {
+    Home: "/",
+    Editor: "/editor",
+    About: "/about",
+    UserList: "/users",
+    UserDetail: "/users/:userId",
+  },
+  {
+    basePath: "/bike-ride-planner/",
+  }
+);
